c6bf8d181b328b72bee2c682422b1f8ce27d3314
Wonseok SEO <dydrlaks@likelion.org>
2017-04-14 14:58:55 +0000

    first commit

--- app/controllers/lotto_controller.rb
+++ app/controllers/lotto_controller.rb
@@ -15,20 +15,30 @@ class LottoController < ApplicationController
         end
         
         
-        if params[:input] == est_hash2[0]
-            answer = '정답입니다'
-        else
-            answer = '오답입니다'
-        end 
-        
-       
-        
-    
-        @result = params[:input]
-        @answer = answer
         @result_test_hash2 = est_hash2
         @result_test_hash = est_hash
       
+    end
+    def keyboard
+        @name = params[:input]
+        est_hash = []
+        est_hash2 = []
+        testhash = {"abc" => "abcabc", "qwe" => "qweqwe"}
+        
+        test_hash = testhash.to_a.sample(1).to_h
+        test_hash.each do |x,y| 
+        est_hash << y 
+        est_hash2 << x
+        end
+        
+    if @name == est_hash2[0]
+            answer = '정답입니다'
+        else
+            answer = '오답입니다'
+    end     
+    
+    @answer = answer
+        
     end
 end
 
