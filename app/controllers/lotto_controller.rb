77f7b77e56de6ad325168cab27c9ca9581fce083
Wonseok SEO <dydrlaks@likelion.org>
2017-04-14 10:19:24 +0000

    first commit

--- app/controllers/lotto_controller.rb
+++ app/controllers/lotto_controller.rb
@@ -1,6 +1,36 @@
 class LottoController < ApplicationController
     def index
-        @like = "ddd"
-        @lion = "aaa"
+        
+    end
+    def mouse
+        
+        input_name =  params[:input]
+        
+        est_hash = []
+        est_hash2 = []
+        testhash = {"abc" => "abcabc", "qwe" => "qweqwe"}
+        
+        test_hash = testhash.to_a.sample(1).to_h
+        test_hash.each do |x,y| 
+        est_hash << y 
+        est_hash2 << x
+        end
+        
+        
+        if input_name == est_hash2[0]
+            answer = '정답입니다'
+        else
+            answer = '오답입니다'
+        end 
+    
+        @result = input_name
+        @answer = answer
+        @result_test_hash2 = est_hash2
+        @result_test_hash = est_hash
+      
+    end
+    def keyboard
+        @result_test_hash = est_hash
     end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   get 'lotto/index' => 'lotto#index'
+  get 'lotto/mouse' => 'lotto#mouse'
+  get 'lotto/keyboard' => 'lotto#keyboard'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
